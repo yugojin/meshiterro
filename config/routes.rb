d323dcced8f1afd6de6b7d42f9ed9cbac1de25e8
EC2 Default User <ec2-user@ip-172-31-53-127.ec2.internal>
2020-10-08 08:41:02 +0000

    [Update]部分共通手前まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
